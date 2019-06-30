a521d3086a4fb0de8b1a962891b0859ea8c2753f
Kyle Krick <krkrick@gmail.com>
2019-06-30 16:48:33 -0700

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OutOfOffice.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
