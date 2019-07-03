38e013d4a22aaf09735f660f1feb9fb9ac4557a2
Kyle Krick <krkrick@gmail.com>
2019-07-02 23:35:32 -0700

    Created db and forms for data manipulation

--- Controllers/OutOfOfficeController.cs
+++ Controllers/OutOfOfficeController.cs
@@ -1,13 +1,152 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using OutOfOffice.Models;
 
 namespace OutOfOffice.Controllers
 {
     public class OutOfOfficeController : Controller
     {
-        // Get OutOfOffice
-        public IActionResult Index() {
+        private readonly OutOfOfficeContext _context;
+
+        public OutOfOfficeController(OutOfOfficeContext context)
+        {
+            _context = context;
+        }
+
+        // GET: OutOfOffice
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Request.ToListAsync());
+        }
+
+        // GET: OutOfOffice/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var request = await _context.Request
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (request == null)
+            {
+                return NotFound();
+            }
+
+            return View(request);
+        }
+
+        // GET: OutOfOffice/Create
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        // POST: OutOfOffice/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Email,ApproverEmail,RequestType,LeaveDateTime,ReturnDateTime,Reason,Status")] Request request)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(request);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(request);
+        }
+
+        // GET: OutOfOffice/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var request = await _context.Request.FindAsync(id);
+            if (request == null)
+            {
+                return NotFound();
+            }
+            return View(request);
+        }
+
+        // POST: OutOfOffice/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Email,ApproverEmail,RequestType,LeaveDateTime,ReturnDateTime,Reason,Status")] Request request)
+        {
+            if (id != request.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(request);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RequestExists(request.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(request);
+        }
+
+        // GET: OutOfOffice/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var request = await _context.Request
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (request == null)
+            {
+                return NotFound();
+            }
+
+            return View(request);
+        }
+
+        // POST: OutOfOffice/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var request = await _context.Request.FindAsync(id);
+            _context.Request.Remove(request);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RequestExists(int id)
+        {
+            return _context.Request.Any(e => e.Id == id);
         }
     }
 }
---
+++ Migrations/20190703043539_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OutOfOffice.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Request",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    ApproverEmail = table.Column<string>(nullable: true),
+                    RequestType = table.Column<string>(nullable: true),
+                    LeaveDateTime = table.Column<DateTime>(nullable: false),
+                    ReturnDateTime = table.Column<DateTime>(nullable: false),
+                    Reason = table.Column<string>(nullable: true),
+                    Status = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Request", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Request");
+        }
+    }
+}
