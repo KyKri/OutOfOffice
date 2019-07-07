3f94f35d497ae8d79939b75c9c7000d6fb09b236
Kyle Krick <krkrick@gmail.com>
2019-07-07 16:38:35 -0700

    Added data annotations to change field display names

--- Models/Request.cs
+++ Models/Request.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OutOfOffice.Models
 {
@@ -8,10 +9,14 @@ namespace OutOfOffice.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+        [Display(Name = "Approver Email")]
         public string ApproverEmail { get; set; }
+        [Display(Name = "Request Type")]
         public string RequestType { get; set; }
+        [Display(Name = "Leave Date Time")]
         [DataType(DataType.DateTime)]
         public DateTime LeaveDateTime { get; set; }
+        [Display(Name = "Return Date Time")]
         [DataType(DataType.DateTime)]
         public DateTime ReturnDateTime { get; set; }
         public string Reason { get; set; }
