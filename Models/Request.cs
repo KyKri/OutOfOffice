8e285da99cddccce7134f036c63fecaec9db64d2
Kyle Krick <krkrick@gmail.com>
2019-07-10 12:52:24 -0700

    Added data validation for fields

--- Models/Request.cs
+++ Models/Request.cs
@@ -7,19 +7,37 @@ namespace OutOfOffice.Models
     public class Request 
     {
         public int Id { get; set; }
+
+        [StringLength(50)]
+        [Required]
         public string Name { get; set; }
+
+        [StringLength(50)]
+        [Required]
         public string Email { get; set; }
+
         [Display(Name = "Approver Email")]
+        [StringLength(50)]
+        [Required]
         public string ApproverEmail { get; set; }
+
         [Display(Name = "Request Type")]
+        [Required]
         public string RequestType { get; set; }
+
         [Display(Name = "Leave Date Time")]
         [DataType(DataType.DateTime)]
+        [Required]
         public DateTime LeaveDateTime { get; set; }
+
         [Display(Name = "Return Date Time")]
         [DataType(DataType.DateTime)]
+        [Required]
         public DateTime ReturnDateTime { get; set; }
+        
+        [Required]
         public string Reason { get; set; }
+
         public string Status { get; set; }
     }
 }
